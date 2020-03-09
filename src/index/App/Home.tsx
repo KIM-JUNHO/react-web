cbd6fcca2ad0da459543b664b750b4b060ea2f3d
prpr48 <prpr48@naver.com>
2020-03-10 00:43:58 +0900

    modify

--- src/index/App/Home.tsx
+++ src/index/App/Home.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                     href="#"
                     class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                   >
-                    Live demo
+                    More Info
                   </a>
                 </div>
               </div>
